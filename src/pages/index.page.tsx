0085423e4fca2608100f589b6c0fee2bcfbb9b01
Fernando Souza <l.fernando.dev@gmail.com>
2023-10-01 22:26:25 -0300

    add style to home page

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,9 +1,1 @@
-import { Heading, Text } from '@ignite-ui/react'
-
-export default function Home() {
-  return (
-    <div>
-      <Heading as="h1">Teste</Heading>
-    </div>
-  )
-}
+export { default } from './home'
